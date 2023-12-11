9d2097f4ff1ffdcfb690a0409156d2a52d987940
Ken Atopos <z1158229761@outlook.com>
2023-12-11 20:45:13 +0800

    some process

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -20,12 +21,21 @@ const connect = async () => {
 };
 
 // middleware
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+// error handling
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong.";
+
+  return res.status(errorStatus).json(errorMessage);
+});
 
 app.listen(8800, () => {
   connect();
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,32 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("");
+router.get("/some", verifyToken, (req, res, next) => {
+  res.send("hello");
 });
+
+router.get("/some/:id", verifyUser, (req, res, next) => {
+  res.send("some annotation.");
+});
+
+// update
+router.put("/:id", updateUser);
+
+// delete
+router.delete("/:id", deleteUser);
+
+// get
+router.get("/:id", getUser);
+
+// getAll
+router.get("/", getUsers);
 
 export default router;
